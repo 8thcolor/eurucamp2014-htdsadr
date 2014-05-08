gitlabhq/gitlabhq

commit 2b816075dc71dfe8f6f9e5349fdff7f03ad9dad0

Replace backticks with Gitlab::Popen

lib/backup/manager.rb
@@ -8,7 +8,7 @@ module Backup
       s[:db_version]         = "#{ActiveRecord::Migrator.current_version}"
       s[:backup_created_at]  = Time.now
       s[:gitlab_version]     = Gitlab::VERSION
-      s[:tar_version]        = %x{tar --version | head -1}.gsub(/\n/,"")
+      s[:tar_version]        = tar_version
 
       Dir.chdir(Gitlab.config.backup.path)
 
@@ -97,6 +97,11 @@ module Backup
         puts "Hint: git checkout v#{settings[:gitlab_version]}"
         exit 1
       end
+    end
+
+    def tar_version
+      tar_version, _ = Gitlab::Popen.popen(%W(tar --version))
+      tar_version.split("\n").first
     end
   end
 end

app/controllers/admin/background_jobs_controller.rb
@@ -1,5 +1,6 @@
 class Admin::BackgroundJobsController < Admin::ApplicationController
   def show
-    @sidekiq_processes = `ps -U #{Settings.gitlab.user} -o euser,pid,pcpu,pmem,stat,start,command | grep sidekiq | grep -v grep`
+    ps_output, _ = Gitlab::Popen.popen(%W(ps -U #{Settings.gitlab.user} -o euser,pid,pcpu,pmem,stat,start,command))
+    @sidekiq_processes = ps_output.split("\n").grep(/sidekiq/)
   end
 end

config/initializers/2_app.rb
@@ -1,6 +1,6 @@
 module Gitlab
   VERSION = File.read(Rails.root.join("VERSION")).strip
-  REVISION = `git log --pretty=format:'%h' -n 1`
+  REVISION = Gitlab::Popen.popen(%W(git log --pretty=format:%h -n 1)).first.chomp
 
   def self.config
     Settings

lib/gitlab/logger.rb
@@ -11,12 +11,14 @@ module Gitlab
     def self.read_latest
       path = Rails.root.join("log", file_name)
       self.build unless File.exist?(path)
-      logs = `tail -n 2000 #{path}`.split("\n")
+      tail_output, _ = Gitlab::Popen.popen(%W(tail -n 2000 #{path}))
+      tail_output.split("\n")
     end
 
     def self.read_latest_for filename
       path = Rails.root.join("log", filename)
-      logs = `tail -n 2000 #{path}`.split("\n")
+      tail_output, _ = Gitlab::Popen.popen(%W(tail -n 2000 #{path}))
+      tail_output.split("\n")
     end
 
     def self.build

lib/gitlab/upgrader.rb
@@ -42,8 +42,9 @@ module Gitlab
     end
 
     def latest_version_raw
-      git_tags = `git ls-remote --tags origin | grep tags\/v#{current_version.major}`
-      git_tags = git_tags.lines.to_a.select { |version| version =~ /v\d\.\d\.\d\Z/ }
+      remote_tags, _ = Gitlab::Popen.popen(%W(git ls-remote --tags origin))
+      git_tags = remote_tags.split("\n").grep(/tags\/v#{current_version.major}/)
+      git_tags = git_tags.select { |version| version =~ /v\d\.\d\.\d\Z/ }
       last_tag = git_tags.last.match(/v\d\.\d\.\d/).to_s
     end
 

lib/tasks/gitlab/check.rake
@@ -168,7 +168,7 @@ namespace :gitlab do
     def check_migrations_are_up
       print "All migrations up? ... "
 
-      migration_status =  `bundle exec rake db:migrate:status`
+      migration_status, _ = Gitlab::Popen.popen(%W(bundle exec rake db:migrate:status))
 
       unless migration_status =~ /down\s+\d{14}/
         puts "yes".green
@@ -295,7 +295,7 @@ namespace :gitlab do
         "user.email" => Gitlab.config.gitlab.email_from
       }
       correct_options = options.map do |name, value|
-        run("git config --global --get #{name}").try(:squish) == value
+        run(%W(git config --global --get #{name})).try(:squish) == value
       end
 
       if correct_options.all?
@@ -628,7 +628,8 @@ namespace :gitlab do
     end
 
     def sidekiq_process_count
-      `ps ux`.scan(/sidekiq \d+\.\d+\.\d+/).count
+      ps_ux, _ = Gitlab::Popen.popen(%W(ps ux))
+      ps_ux.scan(/sidekiq \d+\.\d+\.\d+/).count
     end
   end
 
@@ -739,7 +740,7 @@ namespace :gitlab do
 
   def check_git_version
     required_version = Gitlab::VersionInfo.new(1, 7, 10)
-    current_version = Gitlab::VersionInfo.parse(run("#{Gitlab.config.git.bin_path} --version"))
+    current_version = Gitlab::VersionInfo.parse(run(%W(#{Gitlab.config.git.bin_path} --version)))
 
     puts "Your git bin path is \"#{Gitlab.config.git.bin_path}\""
     print "Git version >= #{required_version} ? ... "

lib/tasks/gitlab/info.rake
@@ -4,20 +4,20 @@ namespace :gitlab do
     task info: :environment  do
 
       # check if there is an RVM environment
-      rvm_version = run_and_match("rvm --version", /[\d\.]+/).try(:to_s)
+      rvm_version = run_and_match(%W(rvm --version), /[\d\.]+/).try(:to_s)
       # check Ruby version
-      ruby_version = run_and_match("ruby --version", /[\d\.p]+/).try(:to_s)
+      ruby_version = run_and_match(%W(ruby --version), /[\d\.p]+/).try(:to_s)
       # check Gem version
-      gem_version = run("gem --version")
+      gem_version = run(%W(gem --version))
       # check Bundler version
-      bunder_version = run_and_match("bundle --version", /[\d\.]+/).try(:to_s)
+      bunder_version = run_and_match(%W(bundle --version), /[\d\.]+/).try(:to_s)
       # check Bundler version
-      rake_version = run_and_match("rake --version", /[\d\.]+/).try(:to_s)
+      rake_version = run_and_match(%W(rake --version), /[\d\.]+/).try(:to_s)
 
       puts ""
       puts "System information".yellow
       puts "System:\t\t#{os_name || "unknown".red}"
-      puts "Current User:\t#{`whoami`}"
+      puts "Current User:\t#{run(%W(whoami))}"
       puts "Using RVM:\t#{rvm_version.present? ? "yes".green : "no"}"
       puts "RVM Version:\t#{rvm_version}" if rvm_version.present?
       puts "Ruby Version:\t#{ruby_version || "unknown".red}"

lib/tasks/gitlab/task_helpers.rake
@@ -28,7 +28,7 @@ namespace :gitlab do
   # It will primarily use lsb_relase to determine the OS.
   # It has fallbacks to Debian, SuSE, OS X and systems running systemd.
   def os_name
-    os_name = run("lsb_release -irs")
+    os_name = run(%W(lsb_release -irs))
     os_name ||= if File.readable?('/etc/system-release')
                   File.read('/etc/system-release')
                 end
@@ -39,7 +39,7 @@ namespace :gitlab do
     os_name ||= if File.readable?('/etc/SuSE-release')
                   File.read('/etc/SuSE-release')
                 end
-    os_name ||= if os_x_version = run("sw_vers -productVersion")
+    os_name ||= if os_x_version = run(%W(sw_vers -productVersion))
                   "Mac OS X #{os_x_version}"
                 end
     os_name ||= if File.readable?('/etc/os-release')
@@ -80,13 +80,12 @@ namespace :gitlab do
   #
   # see also #run_and_match
   def run(command)
-    unless `#{command} 2>/dev/null`.blank?
-      `#{command}`
-    end
+    output, _ = Gitlab::Popen.popen(command)
+    output
   end
 
   def uid_for(user_name)
-    run("id -u #{user_name}").chomp.to_i
+    run(%W(id -u #{user_name})).chomp.to_i
   end
 
   def gid_for(group_name)
@@ -100,7 +99,7 @@ namespace :gitlab do
   def warn_user_is_not_gitlab
     unless @warned_user_not_gitlab
       gitlab_user = Gitlab.config.gitlab.user
-      current_user = run("whoami").chomp
+      current_user = run(%W(whoami)).chomp
       unless current_user == gitlab_user
         puts "#{Colored.color(:black)+Colored.color(:on_yellow)} Warning #{Colored.extra(:clear)}"
         puts "  You are running as user #{current_user.magenta}, we hope you know what you are doing."

8thcolor/eurucamp2014-htdsadr

commit 578347b9ab50a2db08fb571ec24d04cb426c6e42

Apply the locale encoding to `tar --version`
Fixes a bug with non-UTF8 locales introduced by
2b816075dc71dfe8f6f9e5349fdff7f03ad9dad0.

lib/backup/manager.rb
@@ -101,7 +101,7 @@ module Backup
 
     def tar_version
       tar_version, _ = Gitlab::Popen.popen(%W(tar --version))
-      tar_version.split("\n").first
+      tar_version.force_encoding('locale').split("\n").first
     end
   end
 end